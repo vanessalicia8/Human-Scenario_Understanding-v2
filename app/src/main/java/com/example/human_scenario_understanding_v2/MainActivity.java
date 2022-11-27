51114c1c930e4d4457506c0e93125f37fde52a2a
vanessalicia8 <vanessalicia8@gmail.com>
2022-11-27 15:23:33 -0800

    text to speech added to main screen

--- app/src/main/java/com/example/human_scenario_understanding_v2/MainActivity.java
+++ app/src/main/java/com/example/human_scenario_understanding_v2/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.human_scenario_understanding_v2;
 
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import android.content.Intent;
 import android.view.View;
+import android.speech.tts.TextToSpeech;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity{
+    //EditText editText;
+    //Button btnText;
+    TextToSpeech tts;
 
 
 
@@ -17,6 +23,19 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ConstraintLayout lay = (ConstraintLayout) findViewById(R.id.main_layout);
+        String text = "Scene It. Human Scenario Understanding. Tap Anywhere to Continue.";
+        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+
+                // if No error is found then only it will run
+                if(i!=TextToSpeech.ERROR){
+                    // To Choose language of speech
+                    tts.setLanguage(Locale.US);
+                    tts.speak(text,TextToSpeech.QUEUE_FLUSH,null,null);
+                }
+            }
+        });
 
         lay.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,5 +45,7 @@ public class MainActivity extends AppCompatActivity{
 
             }
         });
+
+
     }
 }
